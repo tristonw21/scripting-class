c21c704ad8d43a5a332f6b66a6b851f8a70fc0b6
triston <tristonwhite21@gmail.com>
2025-02-13 11:38:07 -0600

    Completed Assignment 2

---
+++ Assignment 2/Assignment 2.py
@@ -0,0 +1,22 @@
+#Assignment 2 - More Input/Output
+
+fullName = input("Enter your full name: ")
+studentID = input("What is your student ID?: ")
+gradYear = input("What year will you graduate?: ")
+
+num1 = input("This semester, how many classes are you taking you take that are 1 credit hour?: ") 
+num2 = input("This semester, how many classes are you taking you take that are 2 credit hours?: ")
+num3 = input("This semester, how many classes are you taking you take that are 3 credit hours?: ")
+num4 = input("This semester, how many classes are you taking you take that are 4 credit hours?: ")
+
+creditHours = int(num1) + (2 * int(num2)) + (3 * int(num3)) + (4 * int(num4))
+int(creditHours)
+
+print("Hello " + fullName + ", your student ID is " + studentID + " and you will graduate in " + gradYear + ".")
+
+if creditHours > 12:
+    print("You are taking " + str(creditHours) + " credit hours this semester, you're doing great! Keep it up!")
+elif creditHours < 9:
+    print("You are taking " + str(creditHours) + " credit hours this semester, you could do better than that! :(")
+else:
+    print("You are taking " + str(creditHours) + " credit hours this semester, you're doing okay.")
