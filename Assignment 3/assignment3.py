12a14390f8f2f6e05ece8bab947618865a42d15a
Triston <tristonwhite21@gmail.com>
2025-03-04 14:48:54 -0800

    Completed assignment 3 #Looping it all up Updated the README file to include the new assignment fixed naming scheme to be universal

--- Assignment 3/assignment3.py
+++ Assignment 3/assignment3.py
@@ -1,27 +1,46 @@
-#assignment2.py - Looping it all up
-
-
+#assignment2.py #Looping it all up
 """
 Take the code you made for Python 2 and put it into a loop that repeats until the user says they want to end the program.
 """
 
-fullName = input("Enter your full name: ")
-studentID = input("What is your student ID?: ")
-gradYear = input("What year will you graduate?: ")
 
-num1 = input("This semester, how many classes are you taking you take that are 1 credit hour?: ") 
-num2 = input("This semester, how many classes are you taking you take that are 2 credit hours?: ")
-num3 = input("This semester, how many classes are you taking you take that are 3 credit hours?: ")
-num4 = input("This semester, how many classes are you taking you take that are 4 credit hours?: ")
+def creditHourCalculate(): # Function to calculate credit hours
+    while True:
+        try:
+            num1 = int(input("How many 1-credit hour classes are you taking?: "))
+            num2 = int(input("How many 2-credit hour classes are you taking?: "))
+            num3 = int(input("How many 3-credit hour classes are you taking?: "))
+            num4 = int(input("How many 4-credit hour classes are you taking?: "))
+            break  # Exit loop if all inputs are valid
+        except ValueError:
+            print("Invalid input! Please enter only whole numbers.")
 
-creditHours = int(num1) + (2 * int(num2)) + (3 * int(num3)) + (4 * int(num4))
-int(creditHours)
+    creditHours = num1 + (2 * num2) + (3 * num3) + (4 * num4)
 
-print("Hello " + fullName + ", your student ID is " + studentID + " and you will graduate in " + gradYear + ".")
+    if creditHours > 12:
+        print(f"\nYou are taking {creditHours} credit hours this semester. You're doing great! Keep it up!")
+    elif creditHours < 9:
+        print(f"\nYou are taking {creditHours} credit hours this semester. You could do better than that!")
+    else:
+        print(f"\nYou are taking {creditHours} credit hours this semester. You're doing okay.")
 
-if creditHours > 12:
-    print("You are taking " + str(creditHours) + " credit hours this semester, you're doing great! Keep it up!")
-elif creditHours < 9:
-    print("You are taking " + str(creditHours) + " credit hours this semester, you could do better than that! :(")
-else:
-    print("You are taking " + str(creditHours) + " credit hours this semester, you're doing okay.")
+
+while True: # Main loop
+    print("\nWelcome to the credit hour calculator!")
+    fullName = input("What is your full name?: ")
+    studentID = input("What is your student ID?: ")
+    gradYear = input("What year will you graduate?: ")
+
+    while True:
+        print(f"\nHello {fullName}, your student ID is {studentID} and you will graduate in {gradYear}.")
+        creditHourCalculate()
+
+    
+        resetCondition = input("\nWould you like to calculate again? (yes or no): ").strip().lower()
+        if resetCondition == "no":
+            break  # Exit inner loop and ask if they want to exit the program
+
+    exitCondition = input("\nWould you like to exit the program? (yes or no): ").strip().lower()
+    if exitCondition == "yes":
+        print("\nThank you for using the Credit Hour Calculator! Goodbye!")
+        break 
--- README.txt
+++ README.txt
@@ -5,3 +5,4 @@ This is where Im gonna store the homework for my scripting class
 Completed 
     Assignment 1 -- FirstPyProject.py #Basic Input/Output
     Assignment 2 -- Assignment2.py #More Input/Output
+    Assignmetn 2 -- assignment2.py #Looping it all up
