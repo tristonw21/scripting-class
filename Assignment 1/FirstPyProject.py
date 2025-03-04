2e18ed0e7a8bcf4df32f56ec5f3cd663fd84089d
Triston <143359813+tristonw21@users.noreply.github.com>
2025-03-04 11:23:40 -0600

    Update FirstPyProject.py to add name and assignment descriptions

--- Assignment 1/FirstPyProject.py
+++ Assignment 1/FirstPyProject.py
@@ -1,3 +1,14 @@
+#FirstPyProject.py - Assignment 1
+
+""
+https://www.online-python.com/
+
+Use the above website. It's worked well for me recently and I haven't found an issue with it yet.
+Lets keep it simple for the first assignment. Write a program that asks for the users name, age, 
+and grade classification and then retells that to the user. Copy/Paste the code here. 
+""
+
+
 name = input("What is your name?: ")
 age = input("Hello, " + name + "! How old are you? --> ")
 grade = input("What is your grade classification? --> ")
